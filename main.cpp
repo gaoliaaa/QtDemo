b644d5a288b70c37782cecf73d0a7206142e2125
gaoliaaa <823654795@qq.com>
2018-07-12 14:41:36 +0800

    test

---
+++ main.cpp
@@ -0,0 +1,13 @@
+﻿#include "widget.h"
+#include <QApplication>
+#include "odbchelper.h"
+
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    OdbcHelper helper;
+    helper.getPointId();
+
+    return a.exec();
+}
---
+++ odbchelper.cpp
@@ -0,0 +1,30 @@
+﻿#include "odbchelper.h"
+
+OdbcHelper::OdbcHelper()
+{
+
+    this->db=QSqlDatabase::addDatabase("QODBC");
+    QString conn="Driver={PostgreSQL UNICODE(x64)};Server=127.0.0.1;Port=5432;Uid=postgres;Pwd=postgres;Database=work";
+    this->db.setDatabaseName(conn);
+    if(!this->db.open())
+    {
+        QMessageBox::critical(0,QString ("odbc error"),QString("??open db error").arg(this->db.lastError().text()));
+        return ;
+
+    }
+}
+
+void OdbcHelper::getPointId(){
+    QSqlQuery *query=new QSqlQuery(db);
+    QString sql="select id from mycollardata order by id";
+//    QString sql="insert into mycollardata values(?,?,?,?,?,?,?,?,?)";
+//    query->prepare(sql);
+//    query->bindValue(0,110);
+//    query->bindValue(1,200202);
+    query->exec(sql);
+
+    while(query->next()){
+        qDebug()<<query->value(0).toInt();
+    }
+    query->~QSqlQuery();
+}
